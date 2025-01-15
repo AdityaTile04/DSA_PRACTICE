7c8752a9f776845337df66569f2d2feb74a78f0c
Aditya Tile <adityatile99@gmail.com>
2025-01-15 13:27:04 +0530

    write optimized code for second largest element in an array

--- Array/Easy/SecondLargest.java
+++ Array/Easy/SecondLargest.java
@@ -2,7 +2,7 @@ package Array.Easy;
 
 public class SecondLargest {
 
-    public static int secondLargest(int n[]) {
+    public static int betterApproach(int n[]) {
         // Better Approach
         int largest = Integer.MIN_VALUE;
         for (int i = 0; i < n.length; i++) {
@@ -17,10 +17,28 @@ public class SecondLargest {
             }
         }
         return secondLargest;
+
+    }
+
+    // Optimized Approach
+    public static int optimizedApproach(int n[]) {
+        int largest = n[0];
+        int slargest = -1;
+        for (int i = 0; i < n.length; i++) {
+            if (n[i] > largest) {
+                slargest = largest;
+                largest = n[i];
+            } else if (n[i] < largest && n[i] > slargest) {
+                slargest = n[i];
+            }
+        }
+        return slargest;
+
     }
 
     public static void main(String args[]) {
-        int arr[] = { 1, 2, 4, 7, 7, 5 };
-        System.out.println("Second largest is : " + secondLargest(arr));
+        int arr[] = { 10, 10, 10 };
+        System.out.println("Second largest is : " + betterApproach(arr));
+        System.out.println("Second largest is : " + optimizedApproach(arr));
     }
 }
